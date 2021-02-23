6f9497c097a086b843ea47483151845103f9e4d7
Patryk Agata <patrykagata@wp.pl>
2021-02-23 13:50:17 +0100

    Dodanie możliwości ponowienia działania aplikacji

--- Sente_ConsoleApp/Program.cs
+++ Sente_ConsoleApp/Program.cs
@@ -25,27 +25,53 @@ namespace Sente_ConsoleApp
 
         #endregion
 
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Twórca : Patryk Agata");
             Console.WriteLine();
 
-            //Sprawdzenie czy wymagane pliki znajdują się w folderze aplikacji
-            if (!File.Exists(file_piramida_nazwa) || !File.Exists(file_przelewy_nazwa))
+            while (true)
             {
-                Console.WriteLine("Brak wymaganych plików w folderze aplikacji!");
+                //Sprawdzenie czy wymagane pliki znajdują się w folderze aplikacji
+                if (!File.Exists(file_piramida_nazwa) || !File.Exists(file_przelewy_nazwa))
+                {
+                    Console.WriteLine("Brak wymaganych plików w folderze aplikacji!");
+                    Console.WriteLine("Wciśnij ENTER aby spróbować ponownie.");
+                    Console.WriteLine("Wpisz 'exit' a następnie wciśnij ENTER aby wyjść.");
+
+                    //Sprawdzanie czy ponowić działanie aplikacji czy wyjść
+                    var readed_val = Console.ReadLine();
+                    if (readed_val == "exit")
+                    {
+                        break;
+                    }
+                    continue;
+                }
+
+                //Odczyt danych z pliku piramida.xml oraz zaciągniecie ich do Listy "Piramida"
+                Piramida = Functions.Piramida.Process_Piramida(file_piramida_nazwa);
+
+                //Odczyt danych z pliku przelewy.xml oraz przeprocesowanie ich na przelewy
+                Functions.Przelewy.Process_Przelewy(file_przelewy_nazwa, Piramida);
+
+                //Wygenerowanie raportu
+                Functions.Raport.Generate_Raport(Piramida);
+
+                //Komunikat na koniec działania aplikacji
+                Console.WriteLine();
+                Console.WriteLine("Proces zakończony!");
+                Console.WriteLine("Wciśnij ENTER aby ponowić.");
+                Console.WriteLine("Wpisz 'exit' a następnie wciśnij ENTER aby wyjść.");
+
+                //Sprawdzanie czy ponowić działanie aplikacji czy wyjść
+                var readed_val_exit = Console.ReadLine();
+                if (readed_val_exit == "exit")
+                {
+                    break;
+                }
+                continue;
             }
 
-            //Odczyt danych z pliku piramida.xml oraz zaciągniecie ich do Listy "Piramida"
-            Piramida = Functions.Piramida.Process_Piramida(file_piramida_nazwa);
-
-            //Odczyt danych z pliku przelewy.xml oraz przeprocesowanie ich na przelewy
-            Functions.Przelewy.Process_Przelewy(file_przelewy_nazwa, Piramida);
-
-            //Wygenerowanie raportu
-            Functions.Raport.Generate_Raport(Piramida);
-
-            Console.ReadKey();
         }
     }
 }
